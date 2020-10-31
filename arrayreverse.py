4dbaac08bd2703a5f5ca8984d16b1ce4f0bf8468
Aditi219 <as2526diti@gmail.com>
2020-10-31 19:56:21 +0530

    reverse array

--- arrayreverse.py
+++ arrayreverse.py
@@ -1,1 +1,12 @@
-array
+def rev(a, start, end):
+    if start >= end:
+        return
+    a[start], a[end] = a[end], a[start]
+    rev(a, start+1, end-1)
+
+
+if __name__ == "__main__":
+    n = input().split()
+    l = len(n)
+    rev(n, 0, l-1)
+    print(n)
