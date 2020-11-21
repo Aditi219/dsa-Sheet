d9200d3887dcc282834975f3766845c2dd43b02e
Aditi219 <as2526diti@gmail.com>
2020-11-21 17:51:20 +0530

    Reverse k elements in linked list

--- LinkedList/ReverseKelements_E.py
+++ LinkedList/ReverseKelements_E.py
@@ -8,25 +8,50 @@ class LinkedList():
     def __init__(self):
         self.head = None
 
-    def push(self, new_data):  # to insert at the beginning
+    def push(self, new_data):
         new_node = Node(new_data)
         new_node.next = self.head
         self.head = new_node
 
     def reverselinked(self, head, k):
-        current = head
-        prev = None
-        next = None
-        count = 0
-        while(current is not None and count < k):
-            next = current.next
-            current.next = prev
-            prev = current
-            current = next
-            count += 1
-        if next is not None:
-            head.next = self.reverselinked(next, k)
-        return prev
+        if (k == 1):
+            return head
+
+        start_node = head
+        end_node = head
+        previous_start_node = None
+
+        while (1):
+            remaining_nodes = k
+
+            while (remaining_nodes > 0 and end_node != None):
+                end_node = end_node.next
+                remaining_nodes -= 1
+
+            if (remaining_nodes == 0):
+                middle_node = start_node.next
+                reversed_group_head = start_node
+
+                while (middle_node != end_node):
+                    next_middle_node = middle_node.next
+                    middle_node.next = reversed_group_head
+                    reversed_group_head = middle_node
+                    middle_node = next_middle_node
+
+                if (previous_start_node != None):
+                    previous_start_node.next = reversed_group_head
+                else:
+                    head = reversed_group_head
+
+                previous_start_node = start_node
+                start_node = end_node
+            else:
+                if (previous_start_node != None):
+                    previous_start_node.next = start_node
+
+                break
+
+        return head
 
     def printList(self):
         temp = self.head
